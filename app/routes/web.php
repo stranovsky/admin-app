c42b89bd520eb93f777c0bba717310a4a291a795
Timotej Stranovsky <t.stranovsky@azet.sk>
2023-07-06 17:52:55 +0200

    Initial commit

---
+++ app/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+whis be wild card route
+*/
+Route::get('/hello/{id}', function($id) {
+    return response('<h1><i>sup world ' . $id.'</i></h1>');
+})->where('id', '[0-9]+');
+
+//request example
+Route::get('/search', function(Request $request){
+    return($request->username . ' ' . $request->isAdmin);
+});
