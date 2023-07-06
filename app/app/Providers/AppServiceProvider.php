a2f5d860bfaaa2e3cf7b00b74cd8d36f9edc06a9
Timotej Stranovsky <t.stranovsky@azet.sk>
2023-07-06 22:01:02 +0200

    creation of database and first attempt at seeding

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        //this is here for this error
+        //SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 1000 bytes
+        //apparently this is the official solution¯\_(ツ)_/¯
     }
 }
